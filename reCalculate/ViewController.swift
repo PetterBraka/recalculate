911aee150acf10f07d18a1508f3885d1c8dee137
PetterBraka <petter.braka@gmail.com>
2020-03-22 20:10:30 +0100

    Initial Commit

---
+++ reCalculate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  reCalculate
+//
+//  Created by Petter vang Brakalsvålet on 22/03/2020.
+//  Copyright © 2020 Petter vang Brakalsvålet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
